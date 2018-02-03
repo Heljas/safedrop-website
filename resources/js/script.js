3d7573ee303be31facd81a4fe7b279e7e616abde
Heljas <heljas2137@gmail.com>
2018-02-03 02:21:49 +0100

    Added sticky nav, scroll functionality and waypoints

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,47 @@
-$(".faq-question:first .fa-angle-right").toggleClass('rotate');
-$(".faq-answer:first").slideToggle();
+$(".faq-question:first .fa-angle-right").addClass('rotate');
+$(".faq-answer:first").slideDown();
 
 $(".faq-question").on("click", function(){
     $(this).find(".fa-angle-right").toggleClass('rotate');
     $(this).find("+ .faq-answer").slideToggle();
+});
+
+let scrollDuration = 600;
+$("a[href='#top']").on("click", function(){
+    $(window).scrollTo($('header'), {offset: -35, duration: scrollDuration});
+});
+$("a[href='#features']").on("click", function(){
+    $(window).scrollTo($('.features-section'), {offset: -35, duration: scrollDuration});
+});
+$("a[href='#screenshots']").on("click", function(){
+    $(window).scrollTo($('.screens-section'), {offset: -35, duration: scrollDuration});
+});
+$("a[href='#video']").on("click", function(){
+    $(window).scrollTo($('.video-section'), {offset: -35, duration: scrollDuration});
+});
+$("a[href='#faq']").on("click", function(){
+    $(window).scrollTo($('.faq-section'), {offset: -35, duration: scrollDuration});
+});
+$("a[href='#download']").on("click", function(){
+    $(window).scrollTo($('.download-section'), {offset: -35, duration: scrollDuration});
+});
+
+
+$('.features-section').waypoint(function(direction){
+    if(direction === "down"){
+        $("nav").removeClass('fadeOut');
+        $("nav").addClass('sticky-nav');
+    } else {
+        $("nav").addClass("fadeOut");
+    }
+}, {
+    offset: '5%'
+});
+
+$('.features-section').waypoint(function(direction){
+    if(direction === "up"){
+        $("nav").removeClass("fadeOut sticky-nav");
+    }
+}, {
+    offset: '30%'
 });
